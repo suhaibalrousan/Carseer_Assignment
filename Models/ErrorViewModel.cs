564827b161a828147e0f0a6d9db9f7a0236ad816
sohaibalrousan <sjalrousan17@ams.just.edu.jo>
2025-12-29 14:31:03 +0300

    Remove File Not Needed

--- Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace VehicleCatalog.Web.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
